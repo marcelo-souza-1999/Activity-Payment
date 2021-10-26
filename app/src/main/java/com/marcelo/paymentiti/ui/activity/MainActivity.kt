b871b2baa235fb0ecea034ce0542f623ce1249f9
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-26 15:21:47 -0300

    Components of the UI created

--- app/src/main/java/com/marcelo/paymentiti/ui/activity/MainActivity.kt
+++ app/src/main/java/com/marcelo/paymentiti/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.marcelo.paymentiti
+package com.marcelo.paymentiti.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marcelo.paymentiti.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
